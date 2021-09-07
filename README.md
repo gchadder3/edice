eeb5680aadd84696781d9bdd589a3fcab57d81dd
George Chadderdon <gchadder3@gmail.com>
2021-09-07 15:58:45 -0500

    Improved both README.md and the application itself

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # edice Repository
 
-This is a simple demo for using Docker to containerize a normal console Python app.
+This is a simple demo for using Docker to containerize a normal console Python app.  The Docker image
+is [here on Docker Hub](https://hub.docker.com/repository/docker/gchadder3/edice).  To use this (assuming 
+you have a working Docker installation on your machine) do the following:
+* `docker pull gchadder3/edice` to pull the image from Docker Hub.
+* `docker run -i -t gchadder3/edice` to run the application.  (-i = interactive mode, -t = terminal mode)
+
+To install from this repo
+* `git clone https://github.com/gchadder3/edice.git` 
+* `cd edice`
+* `docker build -t edice .` to build the Docker image (`edice`).
+* `docker run -i -t edice` to run it.  The beauty is that you don't have to have Python installed on your 
+machine to run this Python code.  Of course, if you do have Python installed, you could also do `python edice.py` 
+to run it.
+
+The application itself is a simple text console application that may be used for games that require dice, 
+including, for example, polyhedral dice such as are used in paper-based role-playing games such as 
+Dungeons & Dragons.  It starts the user off with a regular die (1d6) and the user can keep rolling the 
+dice or switch to other dice settings (e.g. 3d8: 3 dice of 8 sides each).
--- edice.py
+++ edice.py
@@ -1,5 +1,7 @@
 """
 edice.py -- Simple Python script to simulate electronic gaming dice.
+
+Last updated: 9/7/21 (gchadder3)
 """
 
 import random
@@ -8,28 +10,48 @@ class Dice(object):
     def __init__(self, numDice=1, diceSides=6):
         self.numDice = numDice
         self.diceSides = diceSides
+        self.theDice = [None for ii in range(self.numDice)]  # unrolled dice
         
     def show(self):
-        print('%dd%d' % (self.numDice, self.diceSides), end='')
+        print('%dd%d -- ' % (self.numDice, self.diceSides), end='')
+        if self.theDice[0] is None:
+            print('unrolled')
+        else:
+            for ii in range(self.numDice):
+                print(self.theDice[ii], end='')
+                if ii < (self.numDice - 1):
+                    print(' + ', end='')
+            if self.numDice == 1:
+                print('')
+            else:
+                print(' == %d' % sum(self.theDice))
         
     def roll(self):
         theRoll = 0
         for ii in range(self.numDice):
-            theRoll += random.randint(1, self.diceSides)
+            self.theDice[ii] = random.randint(1, self.diceSides)
+            theRoll += self.theDice[ii]
         return theRoll
         
 if __name__ == '__main__':
     print('E-Dice:')
     print('Starting with a normal die...')
+    
+    # Create a normal 1d6 die.
     theDice = Dice(1, 6)
+
+    # Loop until we break out of the loop...
     while (True):
+        # Roll the dice and show them.
+        theDice.roll()
         theDice.show()
-        print(' -- ', end='')
-        print(theDice.roll())
+        
+        # Query for a command.  By default you can just hit Enter to role the same dice
+        # again.
         inStr = input('Enter a command (q=quit, n=new dice, all other=roll again): ')
-        if inStr == 'q':
+        if inStr == 'q':  # quit
             break
-        elif inStr == 'n':
+        elif inStr == 'n':  # query for new dice settings so user can switch
             numDice = int(input('How many dice to throw?: '))
             numSides = int(input('How many sides on each die?: '))
             theDice = Dice(numDice, numSides)
